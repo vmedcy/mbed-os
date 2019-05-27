52a12d159401267772ffba1d93d0752406c74616
Volodymyr Medvid <volodymyr.medvid@cypress.com>
2019-05-27 16:45:22 +0300

    PSOC6: add WHD EMAC driver

---
+++ features/netsocket/emac-drivers/TARGET_Cypress/TARGET_PSOC6/TARGET_WHD/interface/WhdSTAInterface.cpp
@@ -0,0 +1,324 @@
+/* WHD STAION implementation of NetworkInterfaceAPI
+ * Copyright (c) 2017 ARM Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <cstring>
+#include <algorithm>
+#include "WhdSTAInterface.h"
+#include "nsapi.h"
+#include "lwipopts.h"
+#include "lwip/etharp.h"
+#include "lwip/ethip6.h"
+#include "rtos.h"
+#include "whd_emac.h"
+#include "whd_wifi_api.h"
+
+#define SCAN_RESULT_BUFF_SIZE     (30u)
+#define CMP_MAC( a, b )  (((((unsigned char*)a)[0])==(((unsigned char*)b)[0]))&& \
+                          ((((unsigned char*)a)[1])==(((unsigned char*)b)[1]))&& \
+                          ((((unsigned char*)a)[2])==(((unsigned char*)b)[2]))&& \
+                          ((((unsigned char*)a)[3])==(((unsigned char*)b)[3]))&& \
+                          ((((unsigned char*)a)[4])==(((unsigned char*)b)[4]))&& \
+                          ((((unsigned char*)a)[5])==(((unsigned char*)b)[5])))
+
+struct whd_scan_userdata {
+    Semaphore* sema;
+    WiFiAccessPoint *aps;
+    whd_scan_result_t result_buff[SCAN_RESULT_BUFF_SIZE];
+    unsigned count;
+    unsigned offset;
+    whd_interface_t ifp;
+    bool scan_in_progress;
+};
+
+static whd_scan_userdata interal_scan_data;
+static whd_scan_result_t internal_scan_result;
+static whd_scan_result_t *result_ptr = &internal_scan_result;
+
+extern "C" void whd_emac_wifi_link_state_changed(bool state_up, whd_interface_t ifp);
+
+
+static int whd_toerror(whd_result_t res) {
+    switch (res) {
+        case WHD_NETWORK_NOT_FOUND:             return NSAPI_ERROR_NO_SSID;
+        case WHD_NOT_AUTHENTICATED:
+        case WHD_EAPOL_KEY_PACKET_M3_TIMEOUT:   return NSAPI_ERROR_AUTH_FAILURE;
+        default:                                return -res;
+    }
+}
+
+static nsapi_security_t whd_tosecurity(whd_security_t sec) {
+    switch (sec & (WEP_ENABLED | WPA_SECURITY | WPA2_SECURITY)) {
+        case WEP_ENABLED:   return NSAPI_SECURITY_WEP;
+        case WPA_SECURITY:  return NSAPI_SECURITY_WPA;
+        case WPA2_SECURITY: return NSAPI_SECURITY_WPA2;
+        default:
+            if (sec == WHD_SECURITY_OPEN) return NSAPI_SECURITY_NONE;
+            else                            return NSAPI_SECURITY_UNKNOWN;
+    }
+}
+
+static whd_security_t whd_fromsecurity(nsapi_security_t sec) {
+    switch (sec) {
+        case NSAPI_SECURITY_NONE:       return WHD_SECURITY_OPEN;
+        case NSAPI_SECURITY_WEP:        return WHD_SECURITY_WEP_PSK;
+        case NSAPI_SECURITY_WPA:        return WHD_SECURITY_WPA_MIXED_PSK;
+        case NSAPI_SECURITY_WPA2:       return WHD_SECURITY_WPA2_AES_PSK;
+        case NSAPI_SECURITY_WPA_WPA2:   return WHD_SECURITY_WPA2_MIXED_PSK;
+        default:                        return WHD_SECURITY_UNKNOWN;
+    }
+}
+
+WhdSTAInterface::WhdSTAInterface(WHD_EMAC &emac, OnboardNetworkStack &stack)
+    : EMACInterface(emac, stack),
+      _ssid("\0"),
+      _pass("\0"),
+      _security(NSAPI_SECURITY_NONE),
+      _whd_emac(emac)
+{
+}
+
+nsapi_error_t WhdSTAInterface::connect(
+        const char *ssid, const char *pass,
+        nsapi_security_t security,
+        uint8_t channel)
+{
+    int err = set_channel(channel);
+    if (err) {
+        return err;
+    }
+
+    err = set_credentials(ssid, pass, security);
+    if (err) {
+        return err;
+    }
+
+    return connect();
+}
+
+nsapi_error_t WhdSTAInterface::set_credentials(const char *ssid, const char *pass, nsapi_security_t security)
+{
+    if ((ssid == NULL) ||
+        (strlen(ssid) == 0) ||
+        (pass == NULL && security != NSAPI_SECURITY_NONE) ||
+        (strlen(pass) == 0 && security != NSAPI_SECURITY_NONE) ||
+        (strlen(pass) > 63 && (security == NSAPI_SECURITY_WPA2 || security == NSAPI_SECURITY_WPA || security == NSAPI_SECURITY_WPA_WPA2))
+        )
+    {
+        return NSAPI_ERROR_PARAMETER;
+    }
+
+    memset(_ssid, 0, sizeof(_ssid));
+    strncpy(_ssid, ssid, sizeof(_ssid));
+
+    memset(_pass, 0, sizeof(_pass));
+    strncpy(_pass, pass, sizeof(_pass));
+
+    _security = security;
+
+    return NSAPI_ERROR_OK;
+}
+
+nsapi_error_t WhdSTAInterface::connect()
+{
+
+#define MAX_RETRY_COUNT    ( 5 )
+    int i;
+
+    // initialize wiced, this is noop if already init
+    if (!_whd_emac.powered_up)
+        _whd_emac.power_up();
+
+    if (!_interface) {
+        nsapi_error_t err = _stack.add_ethernet_interface(_emac, true, &_interface);
+        if (err != NSAPI_ERROR_OK) {
+            _interface = NULL;
+            return err;
+        }
+        _interface->attach(_connection_status_cb);
+    }
+
+    if ((_ssid == NULL) ||
+        (strlen(_ssid) == 0)) {
+        return NSAPI_ERROR_PARAMETER;
+    }
+
+    // setup ssid
+    whd_ssid_t ssid;
+    strncpy((char*)ssid.value, _ssid, SSID_NAME_SIZE);
+    ssid.value[SSID_NAME_SIZE-1] = '\0';
+    ssid.length = strlen((char*)ssid.value);
+
+    // choose network security
+    whd_security_t security = whd_fromsecurity(_security);
+
+    // join the network
+    whd_result_t res;
+    for ( i = 0; i < MAX_RETRY_COUNT; i++ )
+    {
+       res = (whd_result_t)whd_wifi_join(_whd_emac.ifp,
+            &ssid,
+            security,
+            (const uint8_t *)_pass, strlen(_pass));
+       if (res == WHD_SUCCESS) {
+           break;
+       }
+    }
+
+    if (res != WHD_SUCCESS) {
+        return whd_toerror(res);
+    }
+
+    /* Use DHCP to get IP address? */
+    set_dhcp( (_ip_address[0] ? false : true) );
+
+    if(whd_wifi_is_ready_to_transceive(_whd_emac.ifp) == WHD_SUCCESS)
+    {
+         whd_emac_wifi_link_state_changed(true, _whd_emac.ifp);
+    }
+
+    // bring up
+    return _interface->bringup(_dhcp,
+            _ip_address[0] ? _ip_address : 0,
+            _netmask[0] ? _netmask : 0,
+            _gateway[0] ? _gateway : 0,
+            DEFAULT_STACK);
+}
+
+
+nsapi_error_t WhdSTAInterface::disconnect()
+{
+    if (!_interface) {
+        return NSAPI_STATUS_DISCONNECTED;
+    }
+
+    // bring down
+    int err = EMACInterface::disconnect();
+    if (err) {
+        return err;
+    }
+
+    // leave network
+    whd_result_t res = whd_wifi_leave(_whd_emac.ifp);
+    if (res != WHD_SUCCESS) {
+        return whd_toerror(res);
+    }
+
+    return NSAPI_ERROR_OK;
+}
+
+int8_t WhdSTAInterface::get_rssi()
+{
+    int32_t rssi;
+    whd_result_t res;
+
+    res = (whd_result_t)whd_wifi_get_rssi(_whd_emac.ifp, &rssi);
+    if (res != 0) {
+        return 0;
+    }
+
+    return (int8_t)rssi;
+}
+
+static void whd_scan_handler(whd_scan_result_t** result_ptr,
+    void* user_data, whd_scan_status_t status)
+{
+
+    whd_scan_userdata *data = (whd_scan_userdata*)user_data;
+
+    /* Even after stopping scan, some results will still come as results are already present in the queue */
+    if (data->scan_in_progress == false) {
+        return;
+    }
+
+    // finished scan, either succesfully or through an abort
+    if (status != WHD_SCAN_INCOMPLETE) {
+        data->scan_in_progress = false;
+        data->sema->release();
+        return;
+    }
+
+    // can't really keep anymore scan results
+    if (data->count > 0 && data->offset >= std::min(data->count, SCAN_RESULT_BUFF_SIZE)) {
+        /* We can not abort the scan as this function is getting executed in WHD context,
+           Note that to call any WHD API, caller function should not in WHD context */
+        return;
+    }
+
+    whd_scan_result_t* record = *result_ptr;
+
+    for (unsigned int i=0; i<data->offset; i++) {
+        if (CMP_MAC( data->result_buff[i].BSSID.octet, record->BSSID.octet ))
+            return;
+    }
+
+    if (data->count > 0) {
+        // get ap stats
+        nsapi_wifi_ap ap;
+
+        uint8_t length = record->SSID.length;
+        if (length < sizeof(ap.ssid)-1) {
+            length = sizeof(ap.ssid)-1;
+        }
+        memcpy(ap.ssid, record->SSID.value, length);
+        ap.ssid[length] = '\0';
+
+        memcpy(ap.bssid, record->BSSID.octet, sizeof(ap.bssid));
+
+        ap.security = whd_tosecurity(record->security);
+        ap.rssi = record->signal_strength;
+        ap.channel = record->channel;
+        data->aps[data->offset] = WiFiAccessPoint(ap);
+    }
+
+    // store to result_buff for future duplication removal
+    data->result_buff[data->offset] = *record;
+    data->offset += 1;
+
+}
+
+
+int WhdSTAInterface::scan(WiFiAccessPoint *aps, unsigned count)
+{
+    // initialize wiced, this is noop if already init
+    if (!_whd_emac.powered_up)
+        _whd_emac.power_up();
+
+    interal_scan_data.sema = new Semaphore();
+    interal_scan_data.aps = aps;
+    interal_scan_data.count = count;
+    interal_scan_data.offset = 0;
+    interal_scan_data.ifp = _whd_emac.ifp;
+    interal_scan_data.scan_in_progress = true;
+    whd_result_t res;
+
+
+    res = (whd_result_t)whd_wifi_scan(_whd_emac.ifp, WHD_SCAN_TYPE_ACTIVE, WHD_BSS_TYPE_ANY,
+        NULL, NULL, NULL, NULL, whd_scan_handler, (whd_scan_result_t **) &result_ptr, &interal_scan_data );
+    if (res != WHD_SUCCESS) {
+        delete interal_scan_data.sema;
+        return whd_toerror(res);
+    }
+
+    int tok = interal_scan_data.sema->wait();
+    if (tok < 1) {
+        delete interal_scan_data.sema;
+        return NSAPI_ERROR_WOULD_BLOCK;
+    }
+
+    delete interal_scan_data.sema;
+    return interal_scan_data.offset;
+}
+
---
+++ features/netsocket/emac-drivers/TARGET_Cypress/TARGET_PSOC6/TARGET_WHD/interface/whd_emac.cpp
@@ -0,0 +1,205 @@
+/*
+ * Copyright (c) 2018 ARM Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "cmsis_os.h"
+#include "whd_emac.h"
+#include "lwip/etharp.h"
+#include "lwip/ethip6.h"
+#include "mbed_shared_queues.h"
+#include "whd_wlioctl.h"
+#include "whd_buffer.h"
+#include "whd_buffer_api.h"
+#include "cybsp_api_wifi.h"
+
+WHD_EMAC::WHD_EMAC(whd_interface_role_t role)
+    : interface_type(role)
+{
+}
+
+WHD_EMAC::WHD_EMAC()
+    : interface_type(WHD_STA_ROLE)
+{
+}
+
+WHD_EMAC &WHD_EMAC::get_instance() {
+    return get_instance(WHD_STA_ROLE);
+}
+
+WHD_EMAC &WHD_EMAC::get_instance(whd_interface_role_t role) {
+    static WHD_EMAC emac_sta(WHD_STA_ROLE);
+    static WHD_EMAC emac_ap(WHD_AP_ROLE);
+    return role == WHD_AP_ROLE ? emac_ap : emac_sta;
+}
+
+uint32_t WHD_EMAC::get_mtu_size() const
+{
+    return WHD_PAYLOAD_MTU;
+}
+
+uint32_t WHD_EMAC::get_align_preference() const
+{
+    return 0;
+}
+
+void WHD_EMAC::add_multicast_group(const uint8_t *addr)
+{
+    memcpy(multicast_addr.octet, addr, sizeof(multicast_addr.octet));
+    whd_wifi_register_multicast_address(ifp, &multicast_addr);
+}
+
+void WHD_EMAC::remove_multicast_group(const uint8_t *addr)
+{
+    whd_wifi_unregister_multicast_address(ifp, &multicast_addr);
+}
+
+void WHD_EMAC::set_all_multicast(bool all)
+{
+    //TODO: new API call for this in the whd interface to be added.
+    //whd_wifi_set_iovar_value(ifp, IOVAR_STR_ALLMULTI, all);
+}
+
+void WHD_EMAC::power_down()
+{
+    powered_up = false;
+    whd_wifi_off(ifp);
+    whd_deinit(ifp);
+}
+
+bool WHD_EMAC::power_up()
+{
+	drvp = *(cybsp_get_wifi_driver());
+	whd_wifi_on(drvp, &ifp /* OUT */);
+    powered_up = true;
+    if (link_state && emac_link_state_cb ) {
+        emac_link_state_cb(link_state);
+    }
+    return true;
+}
+
+bool WHD_EMAC::get_hwaddr(uint8_t *addr) const
+{
+    whd_mac_t mac;
+    whd_result_t res = whd_wifi_get_mac_address(ifp, &mac);
+    MBED_ASSERT(res == WHD_SUCCESS);
+    memcpy(addr, mac.octet, sizeof(mac.octet));
+    return true;
+}
+
+void WHD_EMAC::set_hwaddr(const uint8_t *addr)
+{
+  /* No-op at this stage */
+}
+
+uint8_t WHD_EMAC::get_hwaddr_size() const
+{
+    whd_mac_t mac;
+    return sizeof(mac.octet);
+}
+
+void WHD_EMAC::set_link_input_cb(emac_link_input_cb_t input_cb)
+{
+    emac_link_input_cb = input_cb;
+}
+
+void WHD_EMAC::set_link_state_cb(emac_link_state_change_cb_t state_cb)
+{
+    emac_link_state_cb = state_cb;
+}
+
+void WHD_EMAC::set_memory_manager(EMACMemoryManager &mem_mngr)
+{
+    memory_manager = &mem_mngr;
+}
+
+bool WHD_EMAC::link_out(emac_mem_buf_t *buf)
+{
+    uint16_t offset = 64;
+    whd_buffer_t buffer;
+
+    uint16_t size = memory_manager->get_total_len(buf);
+
+    whd_result_t res = whd_host_buffer_get(drvp, &buffer, WHD_NETWORK_TX, size+offset, WHD_TRUE);
+    MBED_ASSERT(res == WHD_SUCCESS);
+
+    whd_buffer_add_remove_at_front(drvp, &buffer, offset);
+
+    void *dest = whd_buffer_get_current_piece_data_pointer(drvp, buffer);
+    memory_manager->copy_from_buf(dest, size, buf);
+
+    whd_network_send_ethernet_data(ifp, buffer);
+    memory_manager->free(buf);
+    return true;
+}
+
+void WHD_EMAC::get_ifname(char *name, uint8_t size) const
+{
+    memcpy(name, "wl", size);
+}
+
+extern "C"
+{
+void cy_network_process_ethernet_data(whd_interface_t ifp, whd_buffer_t buffer)
+{
+	emac_mem_buf_t *mem_buf = NULL;
+
+    WHD_EMAC &emac = WHD_EMAC::get_instance();
+
+    if (!emac.powered_up && !emac.emac_link_input_cb) {
+        // ignore any trailing packets
+        whd_buffer_release(emac.drvp, buffer, WHD_NETWORK_RX);
+        return;
+    }
+
+    void *data = whd_buffer_get_current_piece_data_pointer(emac.drvp, buffer);
+    uint16_t size = whd_buffer_get_current_piece_size(emac.drvp, buffer);
+
+    if (size > 0) {
+        /* Allocate a memory buffer chain from buffer pool */
+        mem_buf = emac.memory_manager->alloc_heap(size, 0);
+        if (mem_buf != NULL) {
+            memcpy(static_cast<uint8_t *>(emac.memory_manager->get_ptr(mem_buf)), static_cast<uint8_t *>(data), size);
+            emac.emac_link_input_cb(mem_buf);
+        };
+    }
+    whd_buffer_release(emac.drvp, buffer, WHD_NETWORK_RX);
+}
+
+void whd_emac_wifi_link_state_changed(bool state_up, whd_interface_t ifp)
+{
+    WHD_EMAC &emac = WHD_EMAC::get_instance(ifp->role);
+
+    emac.link_state = state_up;
+    if (emac.emac_link_state_cb ) {
+        emac.emac_link_state_cb(state_up);
+    }
+}
+
+//Stubs
+whd_result_t whd_network_deinit           (void) { return WHD_SUCCESS; }
+whd_result_t whd_network_init             (void) { return WHD_SUCCESS; }
+void whd_wireless_link_down_handler         (void) {}
+void whd_wireless_link_renew_handler        (void) {}
+void whd_wireless_link_up_handler           (void) {}
+whd_result_t whd_network_notify_link_up   (void) { return WHD_SUCCESS; }
+whd_result_t whd_network_notify_link_down (void) { return WHD_SUCCESS; }
+} // extern "C"
+
+
